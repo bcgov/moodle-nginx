0d1304c45fa5af46b9de9387bf84951dc8e9bc80
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-05-03 17:50:35 -0700

    Fix conditional statement in deploy-template.sh

--- openshift/scripts/deploy-template.sh
+++ openshift/scripts/deploy-template.sh
@@ -91,7 +91,7 @@ echo "Right-sizing cluster..."
 tail -n +2 ./openshift/${DEPLOY_NAMESPACE}-sizing.csv | while IFS=, read -r Deployment Type PodCount MaxPods PVCCount PVCCapacity CPURequest CPULimit MemRequest MemLimit
 do
     # Ignore if the type is 'job'
-    if [ "$Type" == "sts" || "$Type" == "dc" ]
+    if [["$Type" == "sts" || "$Type" == "dc" ]]
     then
         # Build the command
         cmd="oc set resources $Type $Deployment --limits=cpu=${CPULimit}m,memory=${MemLimit}Mi --requests=cpu=${CPURequest}m,memory=${MemRequest}Mi"
