9b761de6ad088f5d2440dc7c6440f11f45ec54a3
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-05-09 11:06:48 -0700

    Refactor deploy-database.sh to use environment variables for deployment name and namespace

--- openshift/scripts/deploy-database.sh
+++ openshift/scripts/deploy-database.sh
@@ -1,27 +1,27 @@
-if [[ `oc describe sts ${{ inputs.DB_DEPLOYMENT_NAME  }} 2>&1` =~ "NotFound" ]]; then
-  echo "${{ inputs.DB_DEPLOYMENT_NAME  }} NOT FOUND: Beginning deployment..."
-  oc create -f ./config/mariadb/config.yaml -n ${{ inputs.DEPLOY_NAMESPACE }}
+if [[ `oc describe sts $DB_DEPLOYMENT_NAME 2>&1` =~ "NotFound" ]]; then
+  echo "$DB_DEPLOYMENT_NAME NOT FOUND: Beginning deployment..."
+  oc create -f ./config/mariadb/config.yaml -n $DEPLOY_NAMESPACE
 else
-  echo "${{ inputs.DB_DEPLOYMENT_NAME  }} Installation found...Scaling to 0..."
-  oc scale sts ${{ inputs.DB_DEPLOYMENT_NAME  }} --replicas=0
+  echo "$DB_DEPLOYMENT_NAME Installation found...Scaling to 0..."
+  oc scale sts $DB_DEPLOYMENT_NAME --replicas=0
 
   ATTEMPTS=0
   MAX_ATTEMPTS=60
-  while [[ $(oc get sts ${{ inputs.DB_DEPLOYMENT_NAME }} -o jsonpath='{.status.replicas}') -ne 0 && $ATTEMPTS -ne $MAX_ATTEMPTS ]]; do
-    echo "Waiting for ${{ inputs.DB_DEPLOYMENT_NAME }} to scale to 0..."
+  while [[ $(oc get sts $DB_DEPLOYMENT_NAME -o jsonpath='{.status.replicas}') -ne 0 && $ATTEMPTS -ne $MAX_ATTEMPTS ]]; do
+    echo "Waiting for $DB_DEPLOYMENT_NAME to scale to 0..."
     sleep 10
     ATTEMPTS=$((ATTEMPTS + 1))
   done
   if [[ $ATTEMPTS -eq $MAX_ATTEMPTS ]]; then
-    echo "Timeout waiting for ${{ inputs.DB_DEPLOYMENT_NAME }} to scale to 0"
+    echo "Timeout waiting for $DB_DEPLOYMENT_NAME to scale to 0"
     exit 1
   fi
 
-  echo "Recreating ${{ inputs.DB_DEPLOYMENT_NAME  }}..."
-  oc delete sts ${{ inputs.DB_DEPLOYMENT_NAME  }} -n ${{ inputs.DEPLOY_NAMESPACE }}
-  oc delete configmap ${{ inputs.DB_DEPLOYMENT_NAME  }}-config -n ${{ inputs.DEPLOY_NAMESPACE }}
-  oc delete service ${{ inputs.DB_DEPLOYMENT_NAME  }} -n ${{ inputs.DEPLOY_NAMESPACE }}
-  oc create -f ./config/mariadb/config.yaml -n ${{ inputs.DEPLOY_NAMESPACE }}
-  oc annotate --overwrite  sts/${{ inputs.DB_DEPLOYMENT_NAME  }} kubectl.kubernetes.io/restartedAt=`date +%FT%T` -n ${{ inputs.DEPLOY_NAMESPACE }}
-  # oc rollout restart sts/${{ inputs.DB_DEPLOYMENT_NAME  }}
+  echo "Recreating $DB_DEPLOYMENT_NAME..."
+  oc delete sts $DB_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE
+  oc delete configmap $DB_DEPLOYMENT_NAME-config -n $DEPLOY_NAMESPACE
+  oc delete service $DB_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE
+  oc create -f ./config/mariadb/config.yaml -n $DEPLOY_NAMESPACE
+  oc annotate --overwrite  sts/$DB_DEPLOYMENT_NAME kubectl.kubernetes.io/restartedAt=`date +%FT%T` -n $DEPLOY_NAMESPACE
+  # oc rollout restart sts/$DB_DEPLOYMENT_NAME
 fi
