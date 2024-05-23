bde73dac827e900f399e1769845dad828747d144
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-05-23 14:47:51 -0700

    Clean-up any previous Redis deployment before deploying Added REDIS_IMAGE to deploy workflow call

--- openshift/scripts/deploy-redis.sh
+++ openshift/scripts/deploy-redis.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+$route_name=$REDIS_DEPLOYMENT_NAME
+if [[ `oc describe route $route_name 2>&1` =~ "NotFound" ]]; then
+  echo "Route NOT FOUND: $route_name - Skipping..."
+else
+  echo "$route_name FOUND: Cleaning resources..."
+  oc delete route $route_name
+  echo "DELETED route:  $route_name"
+fi
+
+if [[ `oc describe svc/$route_name 2>&1` =~ "NotFound" ]]; then
+  echo "Service NOT FOUND: $route_name - Skipping..."
+else
+  echo "$route_name FOUND: Cleaning resources..."
+  oc delete svc/$route_name
+  echo "DELETED service:  $route_name"
+fi
+
 # Create a headless service to control the domain of the Redis cluster
 oc create service clusterip $REDIS_DEPLOYMENT_NAME --tcp=6379:6379 -n $DEPLOY_NAMESPACE``
 
