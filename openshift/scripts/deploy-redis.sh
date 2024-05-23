7f33fa8d9574d77ea6c95d9900c088a9950abb83
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-05-23 15:20:52 -0700

    Added redis configMap and changed sts config

--- openshift/scripts/deploy-redis.sh
+++ openshift/scripts/deploy-redis.sh
@@ -17,12 +17,23 @@ else
   echo "DELETED service:  $route_name"
 fi
 
+if [[ `oc describe configmap $REDIS_DEPLOYMENT_NAME 2>&1` =~ "NotFound" ]]; then
+  echo "ConfigMap NOT FOUND: $REDIS_DEPLOYMENT_NAME - Skipping..."
+else
+  echo "$REDIS_DEPLOYMENT_NAME FOUND: Cleaning resources..."
+  oc delete configmap $REDIS_DEPLOYMENT_NAME
+  echo "DELETED configmap:  $REDIS_DEPLOYMENT_NAME"
+fi
+
+echo "Creating configMap: $REDIS_DEPLOYMENT_NAME"
+oc apply -f ./config/redis/redis-config.yml
+
 # Create a headless service to control the domain of the Redis cluster
 oc create service clusterip $REDIS_DEPLOYMENT_NAME --tcp=6379:6379 -n $DEPLOY_NAMESPACE``
 
 # Create a StatefulSet for Redis
 echo "Deploy Redis to OpenShift ..."
-envsubst < ./openshift/redis.yml | oc apply -f -
+envsubst < ./openshift/redis-sts.yml | oc apply -f -
 
 # Expose the service
 oc expose svc/$REDIS_DEPLOYMENT_NAME -n $DEPLOY_NAMESPACE
