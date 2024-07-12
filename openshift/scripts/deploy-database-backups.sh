f32b9006e3e2b62e0eea42d6c62971a92694c332
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-07-12 14:07:35 -0700

    Enhance database backup shell to update config when already existing

--- openshift/scripts/deploy-database-backups.sh
+++ openshift/scripts/deploy-database-backups.sh
@@ -5,24 +5,32 @@ echo "Deploying database backups to: $DB_BACKUP_DEPLOYMENT_NAME..."
 
 # Check if the Helm deployment exists
 if helm list -q | grep -q "^$DB_BACKUP_DEPLOYMENT_NAME$"; then
-  echo "Helm deployment FOUND. Updating..."
-  if [[ `helm upgrade $DB_BACKUP_DEPLOYMENT_NAME $BACKUP_HELM_CHART --reuse-values 2>&1` =~ "Error" ]]; then
+  echo "Helm deployment found. Updating..."
+
+  # Create a temporary values file with the updated backupConfig
+  cat <<EOF > temp-values.yaml
+  backupConfig: |
+    mariadb=$DB_HOST:$DB_PORT/$DB_NAME
+    0 1 * * * default ./backup.sh -s
+    0 4 * * * default ./backup.sh -s -v all
+  EOF
+
+  # Upgrade the Helm deployment with the new values
+  if [[ `helm upgrade $DB_BACKUP_DEPLOYMENT_NAME $BACKUP_HELM_CHART --reuse-values -f temp-values.yaml 2>&1` =~ "Error" ]]; then
     echo "Backup container update FAILED."
     exit 1
   fi
 
+  # Clean up the temporary values file
+  rm temp-values.yaml
+
   if [[ `oc describe deployment $DB_BACKUP_DEPLOYMENT_FULL_NAME 2>&1` =~ "NotFound" ]]; then
     echo "Backup Helm exists, but deployment NOT FOUND."
-    # Fail here until we can find a fix
     exit 1
-    # echo "Attempt to fix deploymment..."
-    # oc annotate --overwrite  dc/${{ env.WEB_DEPLOYMENT_NAME  }} kubectl.kubernetes.io/restartedAt=`date +%FT%T` -n ${{ env.OPENSHIFT_DEPLOY_PROJECT }}-${{ github.ref_name }}
-
   else
     echo "Backup deployment FOUND. Updating..."
     oc set image deployment/$DB_BACKUP_DEPLOYMENT_FULL_NAME backup-storage=$DB_BACKUP_IMAGE
   fi
-
   echo "Backup container updates completed."
 else
   echo "Helm $DB_BACKUP_DEPLOYMENT_NAME NOT FOUND. Beginning deployment..."
@@ -49,7 +57,7 @@ else
       usernameKey: database-user
       passwordKey: database-password
 
-    env:
+    env:config-vol
       DATABASE_SERVICE_NAME:
         value: \"$DB_HOST\"
       ENVIRONMENT_FRIENDLY_NAME:
