114da886f442a1d6044d27e44de7a6fd16bf81c2
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-12-11 16:22:23 -0800

    Added scale-up to galera deployment Added database online and data verification checks

--- openshift/scripts/deploy-mariadb-galera.sh
+++ openshift/scripts/deploy-mariadb-galera.sh
@@ -25,16 +25,10 @@ if helm list -q | grep -q "^$DB_DEPLOYMENT_NAME$"; then
 
   # Capture the output of the helm upgrade command into a variable
   helm_upgrade_response=$(helm upgrade $DB_DEPLOYMENT_NAME \
-    oci://registry-1.docker.io/bitnamicharts/mariadb-galera --reuse-values \
+    oci://registry-1.docker.io/bitnamicharts/mariadb-galera \
+    --reuse-values \
     --set rootUser.password=$DB_PASSWORD \
     --set galera.mariabackup.password=$DB_PASSWORD \
-    --set extraVolumeMounts[0].name=prestop-script \
-    --set extraVolumeMounts[0].mountPath=/usr/local/bin/prestop.sh \
-    --set extraVolumeMounts[0].subPath=mariadb-prestop.sh \
-    --set extraVolumeMounts[0].readOnly=true \
-    --set lifecycle.preStop.exec.command[0]="/bin/sh" \
-    --set lifecycle.preStop.exec.command[1]="-c" \
-    --set lifecycle.preStop.exec.command[2]="/usr/local/bin/prestop.sh" \
     -f ./config/mariadb/galera-values.yaml 2>&1)
 
    # Output the response for debugging purposes
@@ -150,3 +144,83 @@ if oc get statefulset $DB_DEPLOYMENT_NAME &> /dev/null; then
 else
   echo "StatefulSet $DB_DEPLOYMENT_NAME not found. Skipping patch."
 fi
+
+sleep 10
+
+oc scale sts/$DB_DEPLOYMENT_NAME --replicas=1
+
+sleep 15
+
+# Wait for the deployment to scale to 1
+ATTEMPTS=0
+MAX_ATTEMPTS=60
+while [[ $(oc get sts $DB_DEPLOYMENT_NAME -o jsonpath='{.status.replicas}') -ne 1 && $ATTEMPTS -ne $MAX_ATTEMPTS ]]; do
+  echo "Waiting for $DB_DEPLOYMENT_NAME to scale to 1..."
+  sleep 10
+  ATTEMPTS=$((ATTEMPTS + 1))
+done
+if [[ $ATTEMPTS -eq $MAX_ATTEMPTS ]]; then
+  echo "Timeout waiting for $DB_DEPLOYMENT_NAME to scale to 1"
+  exit 1
+fi
+
+echo "Checking if the database is online and contains expected Moodle data..."
+ATTEMPTS=0
+WAIT_TIME=10
+MAX_ATTEMPTS=30 # wait up to 5 minutes
+
+# Get the name of the first pod in the StatefulSet
+DB_POD_NAME=""
+until [ -n "$DB_POD_NAME" ]; do
+  ATTEMPTS=$(( $ATTEMPTS + 1 ))
+  DB_POD_NAME=$(oc get pods -l app=$DB_DEPLOYMENT_NAME -o jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}')
+
+  if [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; then
+    echo "Timeout waiting for the pod to have status.phase:Running. Exiting..."
+    exit 1
+  fi
+
+  if [ -z "$DB_POD_NAME" ]; then
+    echo "Waiting for the database pod to be ready... $(($ATTEMPTS * $WAIT_TIME)) seconds..."
+    sleep $WAIT_TIME
+  fi
+done
+
+echo "Database pod name: $DB_POD_NAME has been found and is running."
+
+ATTEMPTS=0
+
+until [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
+  ATTEMPTS=$(( $ATTEMPTS + 1 ))
+  echo "Waiting for database to come online... $(($ATTEMPTS * $WAIT_TIME)) seconds..."
+
+  # Capture the output of the mariadb command
+  OUTPUT=$(oc exec $DB_POD_NAME -- bash -c "mariadb -u root -e 'USE $DB_NAME; SELECT COUNT(*) FROM user;'" 2>&1)
+
+  # Check if the output contains an error
+  if echo "$OUTPUT" | grep -qi "error"; then
+    echo "❌ Database error: $OUTPUT"
+    # exit 1
+  fi
+
+  # Extract the user count from the output
+  CURRENT_USER_COUNT=$(echo "$OUTPUT" | grep -oP '\d+')
+
+  if [ $CURRENT_USER_COUNT -gt 0 ]; then
+    echo "Database is online and contains $CURRENT_USER_COUNT users."
+    echo "Resetting master to avoid repolication issues..."
+    RESET=$(oc exec $DB_POD_NAME -- bash -c "mariadb -u root -e 'RESET MASTER;'" 2>&1)
+    echo "Result: $RESET"
+    break
+  else
+    echo "Database is offline. Attempt $ATTEMPTS out of $MAX_ATTEMPTS."
+    sleep $WAIT_TIME
+  fi
+done
+
+if [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; then
+  echo "❌ Timeout waiting for the database to be online. Exiting..."
+  exit 1
+fi
+
+echo "$DB_NAME Database deployment is complete."
