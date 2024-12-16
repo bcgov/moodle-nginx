8b50778890aa90c917e8abd7a5f8542409888738
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2024-12-16 11:24:12 -0800

    Fix Reset master command for db deploy Changed runs-on to openshift/origin-cli from ubuntu-latest

--- openshift/scripts/deploy-mariadb-galera.sh
+++ openshift/scripts/deploy-mariadb-galera.sh
@@ -215,7 +215,7 @@ until [ $ATTEMPTS -eq $MAX_ATTEMPTS ]; do
   if [ $CURRENT_USER_COUNT -gt 0 ]; then
     echo "Database is online and contains $CURRENT_USER_COUNT users."
     echo "Resetting master to avoid repolication issues..."
-    RESET=$(oc exec $DB_POD_NAME -- bash -c "mariadb -u root -e 'RESET MASTER;'" 2>&1)
+    RESET=$(oc exec $DB_POD_NAME -- bash -c "mariadb -u'$DB_USER' -p'$DB_PASSWORD' -e 'RESET MASTER;'" 2>&1)
     echo "Result: $RESET"
     break
   else
