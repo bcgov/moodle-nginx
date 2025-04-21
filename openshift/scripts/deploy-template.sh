05593abaa88a353d10a6bf68dcc678fe35071dbe
Warren Christian <70002196+warrenchristian1telus@users.noreply.github.com>
2025-04-21 09:46:19 -0700

    Removed wait_for_galera_sync from deploy-template

--- openshift/scripts/deploy-template.sh
+++ openshift/scripts/deploy-template.sh
@@ -8,12 +8,6 @@ test -n $DEPLOY_NAMESPACE
 oc project $DEPLOY_NAMESPACE
 echo "Current namespace is $DEPLOY_NAMESPACE"
 
-# Wait for MariaDB Galera cluster to sync (180 reteies with 10 second wait = 30 min max)
-if ! wait_for_galera_sync "mariadb-galera" "$DEPLOY_NAMESPACE" 180 10; then
-  echo "MariaDB Galera cluster failed to sync. Exiting..."
-  exit 1
-fi
-
 # Enable Moodle maintenance mode
 # Note: Should maybe use cron for this [instead of php pod]
 manage_maintenance_mode \
